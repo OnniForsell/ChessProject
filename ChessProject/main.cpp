1968419254589e1d8b57e91b4085fb6d4e9847f0
Onni Forsell <onni.forsell@gmail.com>
2024-02-21 15:40:10 +0200

    Removed commented out while loop

--- ChessProject/main.cpp
+++ ChessProject/main.cpp
@@ -81,65 +81,8 @@ int main() {
 	std::vector<Move> moves;
 
 
-	// state.give_all_raw_moves(state._current_turn, moves);
-
 	state.give_moves(moves);
 	move_history.push_back(state);
-
-	// float value = State::minimax(state, 2);
-
-	// MinMaxValue value = state.minmax(2);
-
-	// std::cout << value << "\n";
-
-	/* while (moves.size() > 0)
-	{
-		state.print_board();
-
-		std::cout << state.evaluate();
-
-		// Print all possible moves in the following format
-		// e2e4
-		// e2e3
-		// etc
-		std::cout << "Available moves:\n" << "+------+\n";
-		for (const Move& move : moves) {
-			std::string chessMove = move.toChessMove();
-			std::cout << "| " << chessMove << " |\n";
-			std::cout << "+------+" << "\n";
-		}
- 
-		// Print the number of legal moves, fe 20
-		std::cout << "Number of legal moves: " << moves.size() << "\n";
-
-		// Ask the player for their next move
-		std::string next_move;
-		std::cout << "Give your next move: ";
-
-		// Repeat the question until the user inputs a valid move
-		std::cin >> next_move;
-		while (!isValidMove(next_move, moves)) {
-			std::cout << "Invalid move. Please enter a valid move: ";
-			std::cin >> next_move;
-		}
-
-		state.make_move(next_move);
-
-		moves.clear();
-
-		state.print_board();
-
-		MinMaxValue value = state.alphabeta(3, std::numeric_limits<float>::lowest(), -std::numeric_limits<float>::lowest());
-		state.make_move(value._move);
-
-		if (moves.size() <= 0) {
-			std::cout << "Checkmate" << "\n";
-		}
-
-		// state.give_all_raw_moves(state._current_turn, moves);
-
-	} */
-
 
 	while (moves.size() > 0)
 	{
